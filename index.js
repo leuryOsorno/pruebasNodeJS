3dacbb743ad1003ec12ad577aa6387daa749724c
Leury Osorno <leury.osorno@Correo.tdea.edu.co>
2023-03-14 21:21:13 -0500

    creasion de enpoints para pruebas de software

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/node', (req, res) => res.send('Hello Node js'));
+app.get('/node/subnode', (req, res) => res.send('Hello soy un hijo de node'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-console.log('Hola NodeJS');
+console.log('Hola NodeJS pruebas de software');
